f86168560b2c6177f6d680a46cfe89d40e8f72e2
Ishwar Verma <ishwarverma8989@gmail.com>
2025-07-16 20:11:49 +0530

    Add files via upload

---
+++ 3_Indexing_&_Slicing/3.2_indexing.py
@@ -0,0 +1,9 @@
+# How to access a specific element in an array?
+
+import numpy as np
+
+arr = np.array([10,20,30,40,50])
+
+print(arr[0]) #first element
+print(arr[2]) #30
+print(arr[-1]) #last element
---
+++ 3_Indexing_&_Slicing/3.6_reshape.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6])
+reshaped_arr = arr.reshape(2,3) #here 2 is row and 3 is column
+print(reshaped_arr)
