0b9a8cd36837a512e90503aab8905a1c2d5a06bc
Ishwar Verma <ishwarverma8989@gmail.com>
2025-07-16 20:04:38 +0530

    Delete Chapter1 directory

--- Chapter1/1d-Array.py
+++
@@ -1,8 +0,0 @@
-# 1d-Array (One dimensional Array) ->
-
-#It is a list of numbers which is store in single row
-
-import numpy as np
-
-ar_1d = np.array([10,20,30,40,50])
-print(ar_1d)
--- Chapter1/multi_d-Array.py
+++
@@ -1,9 +0,0 @@
-# multi dimesnsional array
-#matrix
-
-import numpy as np
-matrix = np.array([[2,4,6],
-                   [8,10,12]])
-print(matrix)
-
-#Matrix is also a 2d array where numbers are stored in rows and columns.
